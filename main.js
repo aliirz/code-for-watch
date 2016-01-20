c56f2236d6d4b167d2c0afb8ca15d005204b5327
Trent Oswald <trentoswald@therebelrobot.com>
2016-01-20 09:36:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var brigades = {}
 _.forEach(window._brigades, function(brigade){
+  brigade.github = brigade.projects_list_url.split('github.com/')[1]
+  brigade.loaded = m.prop(false)
   brigades[brigade.id] = m.prop(brigade)
 })
 domready(function () {
